b64f55c2f4d67c7b3f0b8e80f98765faa697c8dd
shibayan <me@shibayan.jp>
2019-09-16 21:35:23 +0900

    Adding IOptions<T> support

--- Boilerplate/Startup.cs
+++ Boilerplate/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Boilerplate.Services;
+﻿using Boilerplate.Options;
+using Boilerplate.Services;
 
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Boilerplate.Startup))]
@@ -9,9 +11,21 @@ namespace Boilerplate
 {
     public class Startup : FunctionsStartup
     {
+        public Startup()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+
+            Configuration = config.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddSingleton<IGreetingService, GreetingService>();
+
+            builder.Services.Configure<GreetingOptions>(Configuration.GetSection("Greeting"));
         }
     }
 }
